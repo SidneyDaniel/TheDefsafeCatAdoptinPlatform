156dc94d889afcfd1ee10d3899f5cbfaf573227b
Sid.Daniel <sidneylopes17@gmail.com>
2024-09-19 12:31:23 -0300

    🎉 | First Commit

---
+++ nuxt.config.ts
@@ -0,0 +1,14 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-04-03',
+  devtools: { enabled: true },
+  modules: [
+    '@nuxt/ui',
+    '@nuxtjs/supabase',
+    '@prisma/nuxt',
+    '@pinia/nuxt'
+  ],
+  alias: {
+    pinia: "/node_modules/@pinia/nuxt/node_modules/pinia/dist/pinia.mjs"
+  }
+})
